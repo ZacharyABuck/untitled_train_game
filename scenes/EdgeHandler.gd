d1880b204c85b0f7d4c4be59afd1c12ea9f75dda
Peter <lanoitakude@gmail.com>
2024-04-20 21:09:07 -0700

    cleaning up

--- scenes/EdgeHandler.gd
+++ scenes/EdgeHandler.gd
@@ -6,48 +6,21 @@ extends Node
 func _ready():
 	pass # Replace with function body.
 
-# level-up specific Edge (should just be GAIN an Edge -- it'll level it up if it already exists)
-# loop through Edges and apply their stats to Player.
-# add Edge as child. (takes an Edge from Roster as parameter)
-
-
 func add_edge(edge_reference):
-	print("edge reference: ", edge_reference)
-
 	# loop through all Child Edges to see if one already exists.
 	# if it does exist, increase its level. Otherwise, add new Edge.
 	var existing_edge
 	var existing_edge_found = false
 	for child_edge in self.get_children():
 		if child_edge.edge_name == edge_reference["name"]:
-			print("Matching Edge found: ", child_edge.edge_name)
 			existing_edge = child_edge
 			existing_edge_found = true
 	if existing_edge_found:
 		_increase_edge_level(existing_edge)
-		print(existing_edge.edge_name, " is now level ", existing_edge.edge_level)
 	else:
-		print("Edge to load: ", edge_reference["scene"])
 		var edge_scene = edge_reference["scene"].instantiate()
 		add_child(edge_scene)
-		print("New Edge added: ", edge_scene.edge_name)
 
 func _increase_edge_level(edge : Edge):
 	edge.edge_level += 1
 	edge.handle_level_up()
-
-
-#var new_enemy = EnemyInfo.enemy_roster[type]["scene"].instantiate()
-
-	#
-	#if is_instance_valid(edge):
-		#print("hit is instance valid")
-		#edge.edge_level += 1
-		#edge.handle_level_up()
-		#print("New Edge level = ", edge.edge_level)
-	#else:
-		#print("hit ELSE in handle level-up in levle")
-		#var edge_scene = load("res://scenes/edges/fan_the_hammer.tscn")
-		#edge = edge_scene.instantiate()
-		#new_player.receive_edge(edge)
-	#new_player.refresh_current_ranged_weapon_stats()
